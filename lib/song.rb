6cb0f4d1276e16e24685af357d5d1b18a6938d9b
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-17 02:06:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    artist, song = filename.split(" _ ")
+    artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song
